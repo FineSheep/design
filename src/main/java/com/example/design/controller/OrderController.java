328d66fc4a1c8b90578a3ba503384d363c204393
yang <2804821936@qq.com>
2024-04-01 16:43:38 +0800

    接入支付宝

--- src/main/java/com/example/design/controller/OrderController.java
+++ src/main/java/com/example/design/controller/OrderController.java
@@ -1,12 +1,21 @@
 package com.example.design.controller;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.internal.util.AlipaySignature;
 import com.example.design.service.OrderService;
 import com.example.design.state.stateMacyhinne.pojo.Order;
+import com.example.design.strategy.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * 订单服务
@@ -57,5 +66,41 @@ public class OrderController {
     @PostMapping("receive")
     public Order receive (@RequestParam String orderId){
         return orderService.receive(orderId);
+    }
+
+    @RequestMapping("/alipaycallback")
+    public String alipayCallback(HttpServletRequest request) throws AlipayApiException, UnsupportedEncodingException, UnsupportedEncodingException {
+        // 获取回调信息
+        Map<String, String> params = new HashMap<String, String>();
+        Map<String, String[]> requestParams = request.getParameterMap();
+        for (Iterator<String> iter = requestParams.keySet().iterator(); iter.hasNext();) {
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+            }
+            //避免出现乱码
+            valueStr = new String(valueStr.getBytes("ISO-8859-1"), "UTF-8");
+            params.put(name, valueStr);
+        }
+        //验证签名，确保回调接口真的是支付宝平台触发的
+        boolean signVerified = AlipaySignature.rsaCheckV1(params, Constants.ALIPAY_PUBLIC_KEY, "UTF-8", Constants.SIGN_TYPE); // 调用SDK验证签名
+        //确定是 支付宝平台 发起的回调
+        if(signVerified){
+            // 商户订单号
+            String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"), "UTF-8");
+            System.out.println("=========="+out_trade_no);
+            // 支付宝流水号
+            String trade_no = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"), "UTF-8");
+            // 支付金额
+            float total_amount = Float.parseFloat(new String(request.getParameter("total_amount").getBytes("ISO-8859-1"), "UTF-8"));
+            //进行相关的业务操作
+            Order order = orderService.pay(out_trade_no);
+
+            return "支付成功页面跳转, 当前订单为：" + order;
+        }else{
+            throw new UnsupportedOperationException("callback verify failed!");
+        }
     }
 }
--- src/main/java/com/example/design/strategy/AliPayStrategy.java
+++ src/main/java/com/example/design/strategy/AliPayStrategy.java
@@ -1,5 +1,8 @@
 package com.example.design.strategy;
 
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.request.AlipayTradePagePayRequest;
 import com.example.design.state.stateMacyhinne.pojo.Order;
 
 /**
@@ -11,6 +14,24 @@ import com.example.design.state.stateMacyhinne.pojo.Order;
 public class AliPayStrategy implements StrategyInterface{
     @Override
     public String pay(Order order) {
-        return null;
+        //创建 Alipay client
+        AlipayClient alipayClient = new DefaultAlipayClient(Constants.ALIPAY_GATEWAY,
+                Constants.APP_ID,Constants.APP_PRIVATE_LEY,"JSON","UTF-8",
+                Constants.ALIPAY_PUBLIC_KEY,Constants.SIGN_TYPE);
+        //设置请求参数
+        AlipayTradePagePayRequest payRequest = new AlipayTradePagePayRequest();
+        payRequest.setReturnUrl(Constants.CALLBACK_URL);
+        payRequest.setBizContent("{\"out_trade_no\":\"" + order.getOrderId() + "\","
+                + "\"total_amount\":\"" + order.getPrice() + "\","
+                + "\"subject\":\"" + "test" + "\","
+                + "\"body\":\"" + "商品描述" + "\","
+                + "\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}");
+        //请求
+        try {
+            String result = alipayClient.pageExecute(payRequest,"GET").getBody();
+            return result;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException("Alipay failed! " + e);
+        }
     }
 }
